44143753894a222dc9ed7842bcb1b5e34311416e
Mehmet Ali Tezcan <38626245+mehmet334@users.noreply.github.com>
2025-04-13 16:42:45 +0300

    Update ODEV_1.sql

--- ODEV_1.sql
+++ ODEV_1.sql
@@ -14,5 +14,5 @@ SELECT last_name FROM customer
 WHERE first_name='Mary';
 
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN veriler
-SELECT last_name FROM film
+SELECT * FROM film
 WHERE NOT length>50  AND NOT (rental_rate=2.99 OR rental_rate=4.99);
